99e479b9efdbfe54b7d34b93afc12f2b31cafc2f
Kayla Peck <kaylap@live.unc.edu>
2017-01-26 11:28:20 -0500

    Added initial file for python code

---
+++ RE_check.py
@@ -0,0 +1,12 @@
+"""Script for analyzing the introduction of RE sites during deep mutational scanning
+
+Kayla Peck, 2017. 
+
+For command line arguments, run::
+
+    python RE_check.py -h
+
+"""
+
+
+main() # run the main program
